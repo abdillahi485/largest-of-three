ea4b416cbff6a599b0d0132b0ff4248bd137bbda
Abdillahi485 <daakircadde143@gmail.com>
2023-10-26 05:59:32 +0300

    the largest number of the three numbers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let a=100;
+let b=200;
+let c=300;
+if (a>b && a>c){
+    console.log('The largest numnber is ' +" " +a)
+}else if(b>a && b>c){
+    console.log("The largest number is " + " " + b)
+}else{
+    console.log("The largest number is " +" " + c)
+}
